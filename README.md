99e37082f18c4db90a2849d67026647e652dacda
randyAhmSya <randysaputra60457@gmail.com>
2024-11-25 15:08:41 +0700

    pertama

---
+++ README.md
@@ -0,0 +1,23 @@
+![alt text](image.png)
+gambar di atas merupakan soal ke 1 yang menampilkan title di atas web nya
+![alt text](image-1.png)
+![alt text](image-2.png)
+gambar di atas merupakan soal ke 2 yang menampilkan filter buku pada website googlelapis
+
+
+![alt text](image-3.png)
+![alt text](image-4.png)
+penjelasan yang ada di atas ada di bawah yitu soal ke 3
+catchError((_) {...}): Blok ini menangani kemungkinan kesalahan yang terjadi saat memanggil getData(). Jika terjadi kesalahan (misalnya, masalah jaringan atau kesalahan pada server), kode di dalam catchError akan dieksekusi.
+
+
+.substring(0, 450): Metode substring digunakan untuk mengambil bagian dari string. Dalam hal ini, kita mengambil karakter dari indeks 0 hingga 450.
+
+
+![alt text](image-5.png)
+gambar di atas merupakan pratikum ke 4 
+
+Fungsi count() adalah fungsi asinkron yang menjumlahkan hasil dari tiga fungsi: returnOneAsync, returnTwoAsync, dan returnThreeAsync. Ketiga fungsi ini dipanggil secara berurutan menggunakan await, sehingga total waktu eksekusinya adalah 9 detik (3 detik per fungsi). Setelah selesai, hasilnya diperbarui ke UI menggunakan setState().
+
+Untuk mengoptimalkan waktu, ketiga fungsi bisa dijalankan bersamaan menggunakan Future.wait, sehingga total waktu hanya 3 detik tanpa mengubah hasil akhirnya.
+
---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Randy',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const futurePage(),
+    );
+  }
+}
+
+class futurePage extends StatefulWidget {
+  const futurePage({super.key});
+
+  @override
+  State<futurePage> createState() => _futurePageState();
+}
+
+class _futurePageState extends State<futurePage> {
+  String result = '';
+  Future<Response> getData() async {
+    const authority = 'www.googleapis.com';
+    const path = '/books/v1/volumes/junbDwAAQBAJ';
+    Uri url = Uri.https(authority, path);
+    return http.get(url);
+  }
+
+  Future<int> returnOneAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 1;
+  }
+
+  Future<int> returnTwoAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 2;
+  }
+
+  Future<int> returnThreeAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 3;
+  }
+
+  Future count() async {
+    int total = 0;
+    total = await returnOneAsync();
+    total += await returnTwoAsync();
+    total += await returnThreeAsync();
+    setState(() {
+      result = total.toString();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'back from the future',
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () {
+                count();
+                setState(() {});
+                getData().then((value) {
+                  result = value.body.toString().substring(0, 450);
+                  setState(() {});
+                }).catchError((_) {
+                  result = 'an error occurred';
+                  setState(() {});
+                });
+              },
+              child: const Text('GO!'),
+            ),
+            const Spacer(),
+            Text(result),
+            const Spacer(),
+            const CircularProgressIndicator(),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
